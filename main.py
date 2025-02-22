d2eb2ca8dcca7b63ac241abb0d3d8bffa72a79f6
ZxcLord1k-X <dima.stasuk64@gmail.com>
2025-02-22 15:28:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,158 @@
-from pygame import*
+from pygame import *
 
 
+# клас-батько для спрайтів
 class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (65, 65))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
 
-    def __init__(self, player_image, player_x, player_y, player_speed):
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+
+# нові класи-спадкоємці
+class Player(GameSprite):
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_a] and self.rect.x > 5:
+           self.rect.x -= self.speed
+       if keys[K_d] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+
+
+
+# клас-спадкоємець для спрайта-ворога (переміщається сам)
+class Enemy(GameSprite):
+   direction = "left"
+
+
+   def update(self):
+       if self.rect.x <= 470:
+           self.direction = "right"
+       if self.rect.x >= win_width - 85:
+           self.direction = "left"
+
+
+       if self.direction == "left":
+           self.rect.x -= self.speed
+       else:
+           self.rect.x += self.speed
+
+class Wall(sprite.Sprite):
+    def __init__(self, color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
-        self.speed = player_speed
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
+        self.width = wall_width
+        self. height = wall_height
+        self.image = Surface((self.width, self.height))
+        self.image.fill((color_1, color_2, color_3))
         self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
+        self.rect.x = wall_x
+        self.rect.y = wall_y
 
-    def reset(self):
+    def draw_wall(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 
-win_widht = 700
+# Ігрова сцена:
+win_width = 700
 win_height = 500
 
-window = display.set_mode((win_widht, win_height))
-display.set_captions("Maze")
-background = transform.scale(image.load("background.jpg"), (win_widht, win_height))
+
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+background = transform.scale(image.load("background.jpg"),
+                            (win_width, win_height))
 
 
-player = GameSprite('hero.png', 5, win_height - 80, 4)
-monster = GameSprite('cyborg.png', win_widht - 80, 280, 2)
-final = GameSprite('treasure.png', win_widht - 120, win_height - 80, 0)
+# Змінити класи для персонажів гри:
+player = Player('hero.png', 5, win_height - 80, 4)
+monster = Enemy('cyborg.png', win_width - 80, 280, 2)
+final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
 
+w1 = Wall(154, 205, 50, 100, 20, 450, 10)
+w2 = Wall(154, 205, 50, 100, 480, 350, 10)
+w3 = Wall(154, 205, 50, 100, 20, 10, 380)
+
+font.init()
+font = font.Font(None, 70)
+win = font.render('YOU WIN!', True, (255, 215, 0))
+lose = font.render('YOU LOSE!', True, (180, 0, 0))
 game = True
 clock = time.Clock()
 FPS = 60
 
 
+# new value for end game
+finish = False
+
+
+
+
+# музика
 mixer.init()
 mixer.music.load('jungles.ogg')
 mixer.music.play()
 
+money = mixer.Sound('money.ogg')
+kick = mixer.Sound('kick.ogg')
 
 while game:
-    for e in event.get()
-        if e.type == QUIT:
-            game = False
+   for e in event.get():
+       if e.type == QUIT:
+           game = False
 
-    window.blit(background, (0, 0))
-    player.reset()
-    monster.reset()
 
-    display.update()
-    clock.tick(FPS)
+   # add if
+   if finish != True:
+       # це вже було, тільки засунули в іф
+       window.blit(background, (0, 0))
+       # add update
+       player.update()
+       monster.update()
+
+
+       player.reset()
+       monster.reset()
+       final.reset()
+
+       w1.draw_wall()
+       w2.draw_wall()
+       w3.draw_wall()
+
+   if (sprite.collide_rect(player, monster)
+        or sprite.collide_rect(player, w1)
+        or sprite.collide_rect(player, w2)
+        or sprite.collide_rect(player, w3)):
+       window.blit(lose, (200, 200))
+       if not finish:
+           kick.play()
+           finish = True
+
+   if sprite.collide_rect(player, final):
+       window.blit(win, (200,200))
+       if not finish:
+           money.play()
+           finish = True
+
+
+
+   display.update()
+   clock.tick(FPS)
+
+
